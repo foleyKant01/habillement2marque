a00e1f1023783bcb81e5e8dd645c5157741cb175
foleyKant <krayediego@gmail.com>
2024-04-09 19:14:31 +0200

    ok

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path:'', component: AppComponent,
+  children: [
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  {path: 'user', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)
+  // },
+  // {
+  //   path: 'super-admin',
+  //   loadChildren: () => import('./super-admin/super-admin.module').then((m) => m.SuperAdminModule)
+  // },
+  { path: '**', component: NotfoundComponent }
+  ]
+}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
