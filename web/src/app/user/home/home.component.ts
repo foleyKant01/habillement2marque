a76e807107fcc7d918584c8207d221474e86b9e2
foleyKant <krayediego@gmail.com>
2024-05-06 11:14:40 +0200

    ok

--- web/src/app/user/home/home.component.ts
+++ web/src/app/user/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackService } from 'src/app/back.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  products: any[] = []; // Tableau pour stocker les produits
+  data: any;
+
+  constructor(private router: Router, private route: ActivatedRoute, private http: BackService) { }
+
+  ngOnInit(): void {
+    this.viewallProducts()
+  }
+
+  viewallProducts(): void {
+    this.http.ReadAllProducts().subscribe({
+      next: (response: any) => {
+        this.products = response || []; // Stocker les produits dans le tableau
+        if(response?.products)  {
+          this.data = response?.products
+          // console.log(this.data)
+
+        }
+      },
+      error: (error) => {
+        console.error('Failed to load products:', error);
+      }
+    });
+  }
 
 }
