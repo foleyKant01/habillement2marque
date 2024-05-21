110d45d2f6d0ce059cfda4ec22239f38e43c5009
foleyKant <krayediego@gmail.com>
2024-05-21 17:24:17 +0200

    Suppression de la limite du process_product et ajoue du mail d'Ani pour le test de tous les produits

--- web/src/app/user/user-routing.module.ts
+++ web/src/app/user/user-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { SuivezNousComponent } from './suivez-nous/suivez-nous.component';
 import { ReadSingleProductComponent } from './read-single-product/read-single-product.component';
 import { ProductByFilterComponent } from './product-by-filter/product-by-filter.component';
+import { OffresComponent } from './offres/offres.component';
 
 const routes: Routes = [
   { path:'', component: UserComponent,
@@ -13,10 +13,10 @@ const routes: Routes = [
     { path:'', redirectTo:'home', pathMatch:'full' },
     { path:'home', component: HomeComponent },
     { path:'read-single-product/:pr_uid/:type', component: ReadSingleProductComponent },
-    { path:'contact', component: ContactComponent },
     { path:'suivez-nous', component: SuivezNousComponent },
+    { path:'offres', component: OffresComponent },
     { path:'product_by_filter/:type', component: ProductByFilterComponent },
-    {path: 'nos-articles', loadChildren: () => import('./nos-articles/nos-articles.module').then((m) => m.NosArticlesModule)},
+    { path: 'nos-articles', loadChildren: () => import('./nos-articles/nos-articles.module').then((m) => m.NosArticlesModule) },
   ]
   }
 ];
--- web/src/app/user/user.module.ts
+++ web/src/app/user/user.module.ts
@@ -6,10 +6,10 @@ import { UserComponent } from './user.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from '../include/header/header.component';
 import { FooterComponent } from '../include/footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
 import { SuivezNousComponent } from './suivez-nous/suivez-nous.component';
 import { ReadSingleProductComponent } from './read-single-product/read-single-product.component';
 import { ProductByFilterComponent } from './product-by-filter/product-by-filter.component';
+import { OffresComponent } from './offres/offres.component';
 
 
 @NgModule({
@@ -18,10 +18,11 @@ import { ProductByFilterComponent } from './product-by-filter/product-by-filter.
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ContactComponent,
+    OffresComponent,
     SuivezNousComponent,
     ReadSingleProductComponent,
     ProductByFilterComponent,
+    OffresComponent,
   ],
   imports: [
     CommonModule,
